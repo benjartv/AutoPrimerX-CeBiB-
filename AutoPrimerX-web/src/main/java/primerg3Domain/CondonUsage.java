9b2cc88c0fbfdee47bcff653591a9975ee18abb0
danielVargas <daniel.vargas@usach.cl>
2016-01-17 16:35:40 -0300

    Uso de codones del humano como ejempo

---
+++ AutoPrimerX-web/src/main/java/primerg3Domain/CondonUsage.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerg3Domain;
+/*
+G GGG    669768.00     16.47
+G GGA    669873.00     16.47
+G GGT    437126.00     10.75
+G GGC    903565.00     22.22
+ 
+E GAG   1609975.00     39.59
+E GAA   1177632.00     28.96
+D GAT    885429.00     21.78
+D GAC   1020595.00     25.10
+ 
+V GTG   1143534.00     28.12
+V GTA    287712.00      7.08
+V GTT    448607.00     11.03
+V GTC    588138.00     14.46
+ 
+A GCG    299495.00      7.37
+A GCA    643471.00     15.82
+A GCT    750096.00     18.45
+A GCC   1127679.00     27.73
+ 
+R AGG    486463.00     11.96
+R AGA    494682.00     12.17
+S AGT    493429.00     12.13
+S AGC    791383.00     19.46
+ 
+K AAG   1295568.00     31.86
+K AAA    993621.00     24.44
+N AAT    689701.00     16.96
+N AAC    776603.00     19.10
+ 
+M ATG    896005.00     22.04
+I ATA    304565.00      7.49
+I ATT    650473.00     16.00
+I ATC    846466.00     20.82
+ 
+T ACG    246105.00      6.05
+T ACA    614523.00     15.11
+T ACT    533609.00     13.12
+T ACC    768147.00     18.89
+ 
+W TGG    535595.00     13.17
+C TGT    430311.00     10.58
+C TGC    513028.00     12.62
+ 
+Y TAT    495699.00     12.19
+Y TAC    622407.00     15.31
+ 
+L TTG    525688.00     12.93
+L TTA    311881.00      7.67
+F TTT    714298.00     17.57
+F TTC    824692.00     20.28
+ 
+S TCG    179419.00      4.41
+S TCA    496448.00     12.21
+S TCT    618711.00     15.22
+S TCC    718892.00     17.68
+ 
+R CGG    464485.00     11.42
+R CGA    250760.00      6.17
+R CGT    184609.00      4.54
+R CGC    423516.00     10.42
+ 
+Q CAG   1391973.00     34.23
+Q CAA    501911.00     12.34
+H CAT    441711.00     10.86
+H CAC    613713.00     15.09
+ 
+L CTG   1611801.00     39.64
+L CTA    290751.00      7.15
+L CTT    536515.00     13.19
+L CTC    796638.00     19.59
+ 
+P CCG    281570.00      6.92      
+P CCA    688038.00     16.92      
+P CCT    713233.00     17.54 
+P CCC    804620.00     19.79
+*/
+public class CondonUsage {
+
+    // Esta variable guarda los nombres que significan los id de los aminoácidos
+    public static final String [][] sinngleletter = {
+        {"G","Glycine","Gly"},
+        {"A","Alanine","Ala"},
+        {"L","Leucine","Leu"},
+        {"M","Methionine","Met"},
+        {"F","Phenylalanine","Phe"},
+        {"W","Tryptophan","Trp"},
+        {"K","Lysine","Lys"},
+        {"E","Glutamic Acid","Glu"},
+        {"S","Serine","Ser"},
+        {"V","Valine","Val"},
+        {"I","Isoleucine","Ile"},
+        {"C","Cysteine","Cys"},
+        {"Y","Tyrosine","Tyr"},
+        {"H","Histidine","His"},
+        {"R","Arginine","Arg"},
+        {"N","Asparagine","Asn"},
+        {"D","Aspartic Acid","Asp"},
+        {"T","Threonine","Thr"}
+    };
+}
+
+
+
+
+
+
+
+
+
