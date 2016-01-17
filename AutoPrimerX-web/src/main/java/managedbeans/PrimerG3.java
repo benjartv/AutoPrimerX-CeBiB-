f114f474ba609ea70b651460c967a5f6d50c4c78
Daniel <daniel.vargas@usach.cl>
2016-01-17 13:29:14 -0300

    Ingresada Nomenclatura IUPAC

--- AutoPrimerX-web/src/main/java/managedbeans/PrimerG3.java
+++ AutoPrimerX-web/src/main/java/managedbeans/PrimerG3.java
@@ -7,15 +7,19 @@ package managedbeans;
 
 import managedbeans.util.JsfUtil;
 import java.io.Serializable;
+import java.util.ArrayList;
 import javax.inject.Named;
 import javax.faces.bean.ManagedBean;
+import primerg3Domain.NomenclaturaIUPAC;
 
 @Named(value = "primerG3")
 @ManagedBean
 public class PrimerG3 {
     private String sequences;
     private String log;
-
+    public ArrayList<NomenclaturaIUPAC> iupac;
+    
+    
     public String getLog() {
         return log;
     }
@@ -33,18 +37,59 @@ public class PrimerG3 {
     }
     
     public void submit(){
+        createIUPAC();
+        for (int i = 0; i< iupac.size(); i++) {
+            System.out.println(iupac.get(i).getNombreletra());
+        }
         String [] sequencesSplit = sequences.split("\n");
+        for (int i = 0; i < sequencesSplit.length; i++) {
+            sequencesSplit[i] = sequencesSplit[i].trim();
+        }
         Integer largo = sequencesSplit[0].length();
-        System.out.println(largo);
-        // La ultima secuencia marca largo -1 no entiendo porque
         for (int i = 1; i < sequencesSplit.length; i++) {
-               System.out.println("aa " + sequencesSplit[i].length() +" " +sequencesSplit[i] );
                if(largo != sequencesSplit[i].length()) {
                    log = "Las sequiencias deben tener el mismo largo";
                    break;
             } else log = "OK";
         }
     
+    }
+    
+    public void createIUPAC(){
+            iupac = new ArrayList();
+                /*
+    A Adenina
+    C Citocina
+    G Guanina
+    T o U Timina o Uracilo
+    R A o G
+    Y C o T
+    S G o C
+    W A o T
+    K G o T
+    M A o C
+    B C o G o T
+    D A o G o T
+    H A o C o T
+    V A o C o G
+    N Cualquiera
+    */
+            iupac.add(new NomenclaturaIUPAC("A",1,0,0,0));
+            iupac.add(new NomenclaturaIUPAC("C",0,1,0,0));
+            iupac.add(new NomenclaturaIUPAC("G",0,0,1,0));
+            iupac.add(new NomenclaturaIUPAC("ToU",0,0,0,1));
+            iupac.add(new NomenclaturaIUPAC("R",1,0,1,0));
+            iupac.add(new NomenclaturaIUPAC("Y",0,1,0,1));
+            iupac.add(new NomenclaturaIUPAC("S",0,1,1,0));
+            iupac.add(new NomenclaturaIUPAC("W",1,0,0,1));
+            iupac.add(new NomenclaturaIUPAC("K",0,0,1,1));
+            iupac.add(new NomenclaturaIUPAC("M",1,1,0,0));
+            iupac.add(new NomenclaturaIUPAC("B",0,1,1,1));
+            iupac.add(new NomenclaturaIUPAC("D",1,0,1,1));
+            iupac.add(new NomenclaturaIUPAC("H",1,1,0,1));
+            iupac.add(new NomenclaturaIUPAC("V",1,1,1,0));
+            iupac.add(new NomenclaturaIUPAC("N",1,1,1,1));
+            
     }
     
 }
---
+++ AutoPrimerX-web/src/main/java/primerg3Domain/NomenclaturaIUPAC.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerg3Domain;
+
+/**
+ *
+ * @author Dany
+ */
+public class NomenclaturaIUPAC {
+    private String nombreletra;
+    private Integer A;
+    private Integer C;
+    private Integer G;
+    private Integer ToU;
+
+    public NomenclaturaIUPAC(String nombreletra, Integer A, Integer C, Integer G, Integer ToU) {
+        this.nombreletra = nombreletra;
+        this.A = A;
+        this.C = C;
+        this.G = G;
+        this.ToU = ToU;
+    }
+
+    public String getNombreletra() {
+        return nombreletra;
+    }
+
+    public void setNombreletra(String nombreletra) {
+        this.nombreletra = nombreletra;
+    }
+
+    public Integer getA() {
+        return A;
+    }
+
+    public void setA(Integer A) {
+        this.A = A;
+    }
+
+    public Integer getC() {
+        return C;
+    }
+
+    public void setC(Integer C) {
+        this.C = C;
+    }
+
+    public Integer getG() {
+        return G;
+    }
+
+    public void setG(Integer G) {
+        this.G = G;
+    }
+
+    public Integer getToU() {
+        return ToU;
+    }
+
+    public void setToU(Integer ToU) {
+        this.ToU = ToU;
+    }
+    
+    
+}
