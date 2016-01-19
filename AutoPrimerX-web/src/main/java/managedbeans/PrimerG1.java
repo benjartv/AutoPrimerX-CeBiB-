a383bd8d9b7d153ed4d91fc108465dad18e42596
MacBookPro <MacBookPro@158.170.93.145>
2016-01-19 17:50:42 -0300

    Calculo preliminar de primers

--- AutoPrimerX-web/src/main/java/managedbeans/PrimerG1.java
+++ AutoPrimerX-web/src/main/java/managedbeans/PrimerG1.java
@@ -7,8 +7,11 @@ package managedbeans;
 
 import managedbeans.util.JsfUtil;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Named;
 import javax.faces.bean.ManagedBean;
+import primerg1Domain.Primer;
 
 /**
  *
@@ -22,22 +25,42 @@ public class PrimerG1{
      * Creates a new instance of primerG1
      */
     private String sequence;
-    private String rev_sequence;
+    private String com_sequence;
+    private int size_seq;
+    private int largo1;
+    private int largo2;
     
-    private String text;
+    private List<Primer> primers_seq;
+    private List<Primer> primers_com;
+    
+    private Primer service = new Primer();
     
     public PrimerG1() {
     }
 
-    public String getText() {
-        return text;
+    public int getSize_seq() {
+        return size_seq;
     }
 
-    public void setText(String text) {
-        this.text = text;
+    public void setSize_seq(int size_seq) {
+        this.size_seq = size_seq;
     }
 
-        
+    public int getLargo1() {
+        return largo1;
+    }
+
+    public void setLargo1(int largo1) {
+        this.largo1 = largo1;
+    }
+
+    public int getLargo2() {
+        return largo2;
+    }
+
+    public void setLargo2(int largo2) {
+        this.largo2 = largo2;
+    }
     
     public String getSequence() {
         return sequence;
@@ -47,35 +70,40 @@ public class PrimerG1{
         this.sequence = sequence;
     }
 
-    public String getRev_sequence() {
-        return rev_sequence;
+    public String getCom_sequence() {
+        return com_sequence;
     }
 
-    public void setRev_sequence(String rev_sequence) {
-        this.rev_sequence = rev_sequence;
+    public void setCom_sequence(String com_sequence) {
+        this.com_sequence = com_sequence;
     }
+
+    public List<Primer> getPrimers_seq() {
+        return primers_seq;
+    }
+
+    public void setPrimers_seq(List<Primer> primers_seq) {
+        this.primers_seq = primers_seq;
+    }
+
+    public List<Primer> getPrimers_com() {
+        return primers_com;
+    }
+
+    public void setPrimers_com(List<Primer> primers_com) {
+        this.primers_com = primers_com;
+    }
+
+
     public void submit(){
-        char[] charArray = new char[sequence.length()];
-        for (int i = 0; i < sequence.length(); i++) {
-            if (sequence.charAt(i) == 'A') {
-                charArray[i] = 'T';
-            }
-            else{
-                if (sequence.charAt(i) == 'T') {
-                    charArray[i] = 'A';
-                }
-                else{
-                    if (sequence.charAt(i) == 'G') {
-                        charArray[i] = 'C';
-                    }
-                    else{
-                        if (sequence.charAt(i) == 'C') {
-                            charArray[i] = 'G';
-                        }
-                    }
-                }
-            }
-        }
-        rev_sequence = new String(charArray);
+        size_seq = sequence.length();
+        com_sequence = service.complemento(sequence);
+        String rev_sequence = new StringBuilder(sequence).reverse().toString();
+        primers_com = service.createPrimer(com_sequence, largo1, largo2);
+        primers_seq = service.createPrimer(rev_sequence, largo1, largo2);
     }
+    
+    
+    
+    
 }
