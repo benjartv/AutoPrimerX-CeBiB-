aac3b94674fd162a72b1c9dac4b63cbe813a3b37
Daniel <daniel.vargas@usach.cl>
2016-02-02 22:47:34 -0300

    CONFIGURACIÓN BASE DE DATOS

---
+++ AutoPrimerX-ejb/src/main/java/entities/TipoOrganismo.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Dany
+ */
+@Entity
+@Table(name = "tipoOrganismo")
+public class TipoOrganismo implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy=IDENTITY)
+    @Column(name = "id_organismo")
+    private Long id;
+
+    private String nombre;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof TipoOrganismo)) {
+            return false;
+        }
+        TipoOrganismo other = (TipoOrganismo) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.tipoOrganismo[ id=" + id + " ]";
+    }
+    
+}
